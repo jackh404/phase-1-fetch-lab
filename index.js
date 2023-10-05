02fbb68faedbfd72c9b66fc9a1e674bad5e2d340
Jack Henza <jackhenza@gmail.com>
2023-10-05 16:35:14 -0400

    saves booklist to global variable for further hijinks

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const books = []
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
   return fetch("https://anapioficeandfire.com/api/books")
   .then(resp => resp.json())
-  .then(data => renderBooks(data))
+  .then(data => {
+    books.push(data)
+    renderBooks(data)
+  })
 }
 
 function renderBooks(books) {
